ab1754e7a32a1c78ba3ffec0f2699011e2c7c207
Silja Petasch <siljatpetasch@gmail.com>
2023-04-22 11:14:12 -0400

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,49 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners();
+
+    for (const account of accounts) {
+        console.log(account.address);
+    }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+    solidity: "0.8.17",
+};
+
+require("@nomicfoundation/hardhat-toolbox");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners();
+
+    for (const account of accounts) {
+        console.log(account.address);
+    }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+    solidity: "0.8.17",
+    networks: {
+        fuji: {
+            url: "https://api.avax-test.network/ext/bc/C/rpc",
+            accounts: ["ea13c75d77fcd1514d10ca10f1c294ec249bc7140bba32c201f828f0ca54f584"],
+      },
+    },
+};
